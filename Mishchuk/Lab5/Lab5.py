2ffa920c8779acac9bdf2ff2d69707347dbe11a5
Vlad Mishchyk <124392538+VladMishchuk@users.noreply.github.com>
2023-04-19 19:13:35 +0300

    Create Lab5.py

---
+++ Mishchuk/Lab5/Lab5.py
@@ -0,0 +1,47 @@
+#1
+def maxVal(lst):
+    max_val = max(lst)
+    if max_val > 0:
+        return max_val
+    else:
+        return "Число менше 0"
+
+my_list = [-1, 3, 7, 2, 5]
+my_list2 = [-1, 0]
+print("1: ", maxVal(my_list))
+print("1: ", maxVal(my_list2))
+
+#2
+def lettersCount(word):
+    return len(word)
+
+my_word = "Hello"
+print("2: ", lettersCount(my_word))
+
+#3
+def power(base, exponent):
+    if exponent >= 0:
+        return base ** exponent
+    else:
+        return "Степінь не може бути від'ємною"
+
+print("3: ", power(2, 3))
+print("3: ", power(3, -2))
+
+#4
+def printNums(n):
+    for i in range(1, n+1):
+        print("  ", i)
+
+print("4: ")
+printNums(7)
+
+#5
+def recArrea(length, height):
+    if (length > 0 and height > 0) and length != height:
+        return length * height
+    else:
+        return "Невірно введені дані"
+
+print("5: ", recArrea(0, 5))
+print("5: ", recArrea(3, 5))
