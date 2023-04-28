20f0258aa45ef8cf80fbaa5dbd9dcbdcc0c56993
Vladyslav Mishchuk <124392538+VladMishchuk@users.noreply.github.com>
2023-04-28 21:19:00 +0000

    Lab 7

--- Mishchuk/Lab7/Lab7.py
+++ Mishchuk/Lab7/Lab7.py
@@ -1,16 +1,27 @@
 import random
-
+import datetime
+import json
+# 1.
 i = 0
 lst = []
 while i < 25:
-    number = random.randint(65, 90) # число від 0 до 100
+    number = random.randint(65, 90)
     lst = lst + [chr(number)]
     i = i+1
-
 f = open('random_string.txt', 'wt')
-
 for i in lst:
-    s = str(i) # конвертувати елемент списку в рядок
-    f.write(s + ' ') # записати числа в рядок
-
+    s = str(i)  # конвертувати елемент списку в рядок
+    f.write(s + ' ')  # записати числа в рядок
 f.close()
+
+# 2.
+users = ["Артур", "Кейт", "Аліса", "Майк"]
+users_data = [{"name": name, "age": random.randint(1, 99)} for name in users]
+data = {
+    "data": users_data,
+    "created_at": datetime.datetime.now().strftime("%d;%m;%y")
+}
+current_date = datetime.datetime.now().strftime("%d;%m;%y")
+file_name = "users_data_"+current_date+".json"
+with open(file_name, 'w') as file:
+    json.dump(data, file, indent=4)
