8a29b06b7b13670d259db744c2d6425a519c0cf0
Anton <122919489+M0sesz@users.noreply.github.com>
2023-06-06 23:24:04 +0300

    Delete 3lab.py

--- 3lab.py
+++
@@ -1,9 +0,0 @@
-my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-new_list = [x for x in my_list if x % 2 != 0]
-print(new_list)
-my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-new_list = [x ** 2 for x in my_list]
-print(new_list)
-my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-max_element = max(my_list)
-print(max_element)
