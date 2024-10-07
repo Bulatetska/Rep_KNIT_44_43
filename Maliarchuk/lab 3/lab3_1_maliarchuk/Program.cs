3b039362957f7a998eea1c5a4ac300a6bee71d28
Dana_boo <malyarchykbohdana2004@gmail.com>
2024-10-07 22:08:58 +0300

    lab 3

---
+++ Maliarchuk/lab 3/lab3_1_maliarchuk/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+
+class Program
+{
+    private class Hero
+    {
+        public string Name { get; set; }
+        public int YearOfBirth { get; set; }
+        public string Comics { get; set; }
+    }
+    private static readonly List<Hero> _heroes = new List<Hero>
+    {
+        new Hero
+        {
+            Name = "Superman",
+            YearOfBirth = 1938,
+            Comics = "Action Comics"
+        },
+
+        new Hero
+        {
+            Name = "Batman",
+            YearOfBirth = 1938,
+            Comics = "Detective Comics"
+        },
+
+        new Hero
+        {
+            Name = "Captain America",
+            YearOfBirth = 1941,
+            Comics = "Captain America Comics"
+        },
+
+        new Hero
+        {
+            Name = "Ironman",
+            YearOfBirth = 1963,
+            Comics = "Tales of Suspense"
+        },
+
+        new Hero
+        {
+            Name = "Spiderman",
+            YearOfBirth = 1963,
+            Comics = "Amazing Fantasy"
+        }
+    };
+
+    static void Main(string[] args)
+    {
+        var heroNames = new List<string>();
+        foreach (var hero in _heroes)
+        {
+            if (hero.Name.Contains("man"))
+            {
+                heroNames.Add(hero.Name);
+            }
+        }
+
+        heroNames.Sort();
+
+        foreach (var heroName in heroNames)
+        {
+            Console.WriteLine(heroName);
+        }
+
+
+
+        Console.WriteLine("\nСупергерої, що народилися в 1941 році:");
+        foreach (var hero in _heroes)
+        {
+            if (hero.YearOfBirth == 1941)
+            {
+                Console.WriteLine(hero.Name);
+            }
+        }
+
+        var comicsNames = new List<string>();
+        foreach (var hero in _heroes)
+        {
+            comicsNames.Add(hero.Comics);
+        }
+
+        comicsNames.Sort();
+
+
+
+        Console.WriteLine("\nКомікси в алфавітному порядку:");
+        foreach (var comics in comicsNames)
+        {
+            Console.WriteLine(comics);
+        }
+
+        var sortedHeroes = _heroes.OrderByDescending(hero => hero.YearOfBirth).ToList();
+
+
+
+        Console.WriteLine("\nСупергерої та роки народження (спадання):");
+        foreach (var hero in sortedHeroes)
+        {
+            Console.WriteLine($"{hero.Name} - {hero.YearOfBirth}");
+        }
+    }
+}
+
---
+++ Maliarchuk/lab 3/lab3_2_maliarchuk/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+
+class Program
+{
+    private class Hero
+    {
+        public string Name { get; set; }
+        public int YearOfBirth { get; set; }
+        public string Comics { get; set; }
+    }
+    private static readonly List<Hero> _heroes = new List<Hero>
+    {
+        new Hero
+        {
+            Name = "Superman",
+            YearOfBirth = 1938,
+            Comics = "Action Comics"
+        },
+
+        new Hero
+        {
+            Name = "Batman",
+            YearOfBirth = 1938,
+            Comics = "Detective Comics"
+        },
+
+        new Hero
+        {
+            Name = "Captain America",
+            YearOfBirth = 1941,
+            Comics = "Captain America Comics"
+        },
+
+        new Hero
+        {
+            Name = "Ironman",
+            YearOfBirth = 1963,
+            Comics = "Tales of Suspense"
+        },
+
+        new Hero
+        {
+            Name = "Spiderman",
+            YearOfBirth = 1963,
+            Comics = "Amazing Fantasy"
+        }
+    };
+
+    static void Main(string[] args)
+    {
+        var heroNames =
+            from hero in _heroes
+            where hero.Name.Contains("man")
+            orderby hero.Name
+            select hero.Name;
+        foreach (var hero in heroNames)
+        {
+            Console.WriteLine(hero);
+        }
+
+
+
+        var heroesBornIn1941 = _heroes
+            .Where(hero => hero.YearOfBirth == 1941)
+            .Select(hero => hero.Name);
+
+        Console.WriteLine("\nСупергерої, що народилися в 1941 році:");
+        foreach (var hero in heroesBornIn1941)
+        {
+            Console.WriteLine(hero);
+        }
+
+
+
+        var sortedComicsNames = _heroes
+            .Select(hero => hero.Comics)
+            .OrderBy(comics => comics);
+
+        Console.WriteLine("\nКомікси в алфавітному порядку:");
+        foreach (var comics in sortedComicsNames)
+        {
+            Console.WriteLine(comics);
+        }
+
+
+
+        var heroesWithBirthYears = _heroes
+            .OrderByDescending(hero => hero.YearOfBirth)
+            .Select(hero => new { hero.Name, hero.YearOfBirth });
+
+        Console.WriteLine("\nСупергерої та роки народження (спадання):");
+        foreach (var hero in heroesWithBirthYears)
+        {
+            Console.WriteLine($"{hero.Name} - {hero.YearOfBirth}");
+        }
+    }
+}
+
