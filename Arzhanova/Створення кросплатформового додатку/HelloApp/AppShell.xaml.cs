147ec8635de4cc6a87ca591fa685853031e46baf
alonaarz <127139827+alonaarz@users.noreply.github.com>
2024-11-05 14:21:00 +0200

    Створення кросплатформового додатку

---
+++ Arzhanova/Створення кросплатформового додатку/HelloApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace HelloApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Arzhanova/Створення кросплатформового додатку/HelloApp/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿namespace HelloApp
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private void OnHelloButtonClicked(object sender, EventArgs e)
+        {
+            HelloButton.Text = "Ви натиснули на кнопку!";
+        }
+
+       
+    }
+
+}
