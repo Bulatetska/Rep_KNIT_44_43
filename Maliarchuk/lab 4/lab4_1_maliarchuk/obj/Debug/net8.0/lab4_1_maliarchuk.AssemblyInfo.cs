8f64a53fdbcb99d5f52a56a18d8ee4411f567b24
Dana_boo <malyarchykbohdana2004@gmail.com>
2024-12-02 14:41:14 +0100

    lab 4

--- Maliarchuk/lab 4/lab4_1_maliarchuk/obj/Debug/net8.0/lab4_1_maliarchuk.AssemblyInfo.cs
+++ Maliarchuk/lab 4/lab4_1_maliarchuk/obj/Debug/net8.0/lab4_1_maliarchuk.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lab4_1_maliarchuk")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b039362957f7a998eea1c5a4ac300a6bee71d28")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c60584ef33815149ebe17ea7d78c60cc768d3ad")]
 [assembly: System.Reflection.AssemblyProductAttribute("lab4_1_maliarchuk")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lab4_1_maliarchuk")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
