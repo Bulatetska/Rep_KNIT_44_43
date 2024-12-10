459eab9de4849e09498d0cb9c8faa88241a4eb8f
alonaarz <127139827+alonaarz@users.noreply.github.com>
2024-12-10 11:25:53 +0200

    Додаток ASP.NET Core MVC

---
+++ Arzhanova/ASP.NET Core MVC/HelloMvcApp/Controllers/HelloController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+public class HelloController : Controller
+{
+    public IActionResult Index()
+    {
+        return Content("Вітаю! Це ваш перший контролер у ASP.NET Core MVC.");
+    }
+}
