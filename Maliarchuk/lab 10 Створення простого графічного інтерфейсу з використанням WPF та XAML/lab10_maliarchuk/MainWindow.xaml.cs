5c2ec3de0bbf36d31abf543e41c1e8e5cac854d6
Dana_boo <malyarchykbohdana2004@gmail.com>
2024-12-15 18:24:13 +0100

    lab 10

---
+++ Maliarchuk/lab 10 Створення простого графічного інтерфейсу з використанням WPF та XAML/lab10_maliarchuk/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace lab10_maliarchuk
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OnConfirmClick(object sender, RoutedEventArgs e)
+        {
+            string name = NameInput.Text;
+
+            if (!string.IsNullOrWhiteSpace(name))
+            {
+                GreetingMessage.Text = $"Привіт, {name}!";
+            }
+            else
+            {
+                GreetingMessage.Text = "Будь ласка, введіть ім'я!";
+            }
+        }
+    }
+}
