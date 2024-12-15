b75847fff9103d0925d8e15266fb06e8aa1b4297
Dana_boo <malyarchykbohdana2004@gmail.com>
2024-12-15 18:40:17 +0100

    lab 13 Створення першого веб-додатку з використанням ASP.NET Core MVC

---
+++ Maliarchuk/lab 13/lab13_maliarchuk/Controllers/HelloController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace lab13_maliarchuk.Controllers
+{
+    public class HelloController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Content("Вітаю! Це ваш перший контролер у ASP.NET Core MVC.");
+        }
+    }
+}
---
+++ Maliarchuk/lab 13/lab13_maliarchuk/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace lab13_maliarchuk.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
